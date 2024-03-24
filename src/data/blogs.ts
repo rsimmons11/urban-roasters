5a27fff18ca33e6289c63d1f6d22267d7061634e
rsimmons11 <ptgrd1980@gmail.com>
2024-03-24 14:43:05 -0700

    updated header name, menu items and blog

--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -6,23 +6,23 @@ import blog3 from "@/assets/blog-3.jpg"
 export const blogs = [
   {
     image: blog1,
-    title: "Tasty and Refreshing",
+    title: "Successful Coffee Shop - How to Turn Visitors into Regulars?",
     author: "Rob Simmons",
     date: "Feb 23, 2024",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat laboriosam aliquam similique tempora ad dicta.",
+    description: "Why some of the coffee shops don't seem to be successful in all the customers and some of them are left without any?",
   },
   {
     image: blog2,
-    title: "Tasty and Refreshing",
+    title: "want to brew better coffee at home",
     author: "Rob Simmons",
-    date: "Feb 23, 2024",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat laboriosam aliquam similique tempora ad dicta.",
+    date: "Nov 18, 2023",
+    description: "Everyone wants to drink great coffee always everywhere, right?",
   },
   {
     image: blog3,
-    title: "Tasty and Refreshing",
-    author: "Rob Simmons",
-    date: "Feb 23, 2024",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat laboriosam aliquam similique tempora ad dicta.",
+    title: "Gifts for coffee lovers",
+    author: "Walther Malone",
+    date: "Oct 23, 2023",
+    description: "Is your friend or a loved one a coffee nerd and you would like to buy him/her something coffee related for a Christmas Gift but don’t know what to buy?",
   },
 ]
---
+++ src/data/menu.ts
@@ -0,0 +1,40 @@
+import menu1 from "@/assets/menu-item-1.jpg"
+import menu2 from "@/assets/menu-item-2.jpg"
+import menu3 from "@/assets/menu-item-3.jpg"
+import menu4 from "@/assets/menu-item-4.jpg"
+import menu5 from "@/assets/menu-item-5.jpg"
+import menu6 from "@/assets/menu-item-6.jpg"
+
+
+export const menu = [
+  {
+    img:  menu1,
+    name: "espresso",
+    price:  4.60
+  },
+  {
+    img:  menu2,
+    name: "Americano",
+    price:  4.70
+  },
+  {
+    img:  menu3,
+    name: "Cappuccino",
+    price:  6.15
+  },
+  {
+    img:  menu4,
+    name: "Espresso Latté",
+    price:  5.70
+  },
+  {
+    img:  menu5,
+    name: "Espresso Brevé",
+    price:  7.80
+  },
+  {
+    img:  menu6,
+    name: "Espresso Mocha",
+    price:  7.15
+  },
+]
